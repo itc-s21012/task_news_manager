162561826e630c2218b727c14642be09fd3a7411
itc-s21012 <s21012@std.it-college.ac.jp>
2022-06-28 14:09:26 +0900

    Initial commit

---
+++ src/main/kotlin/jp/ac/it_college/std/s21012/task_news_manager/TaskNewsManagerApplication.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s21012.task_news_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskNewsManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaskNewsManagerApplication>(*args)
+}
---
+++ src/test/kotlin/jp/ac/it_college/std/s21012/task_news_manager/TaskNewsManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s21012.task_news_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskNewsManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
